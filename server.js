2fb665c8ecc1f12ba77c6071c02195f0ac972c61
kumarram27 <115088025+kumarram27@users.noreply.github.com>
2024-08-08 23:47:55 +0530

    Update server.js to use environment variables for MongoDB URI and port number

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const PORT = process.env.PORT || 5000;
 // MongoDB URI from environment variables
 const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+mongoose.connect(uri);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
